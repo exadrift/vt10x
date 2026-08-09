a44da6ce5078833b8e256efd8603ca1a66c86a04
wire-oss <wire-oss@noreply.github.com>
2026-08-10 06:59:26 +0900

    fixed scrollback logic and added tests

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/exadrift/vt10x
 
 go 1.26.4
 
-require github.com/exadrift/go/ringbuf v0.0.1
+require (
+	github.com/alecthomas/assert v1.0.0
+	github.com/exadrift/go/ringbuf v0.0.1
+)
+
+require (
+	github.com/alecthomas/colour v0.1.0 // indirect
+	github.com/alecthomas/repr v0.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- vt_test.go
+++ vt_test.go
@@ -1,9 +1,12 @@
 package vt10x
 
 import (
+	"fmt"
 	"io"
 	"strings"
 	"testing"
+
+	"github.com/alecthomas/assert"
 )
 
 func extractStr(term Terminal, x0, x1, row int) string {
@@ -60,5 +63,43 @@ func TestNewline(t *testing.T) {
 	attr := term.Cell(cur.X, cur.Y)
 	if attr.FG != DefaultFG {
 		t.Fatal(st.cur.X, st.cur.Y, attr.FG, attr.BG)
+	}
+}
+
+func TestHistoryBuffer(t *testing.T) {
+	term := New(WithSize(10, 5))
+	_, err := term.Write([]byte("\033[20h")) // set CRLF mode
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+	for i := -15; i < 0; i++ {
+		line := fmt.Sprintf("line %d\n", i)
+		_, err := term.Write([]byte(line))
+		assert.NoError(t, err)
+	}
+	_, err = term.Write([]byte("line 0"))
+	assert.NoError(t, err)
+
+	hbLen := term.HistoryBufferLength()
+	//the blank line that gets created at the end when the scroll happens accounts for the extra line
+	assert.Equal(t, 16, hbLen)
+
+	rows := term.AnsiRows()
+	assert.Len(t, rows, 5)
+
+	historyRows := term.History(0)
+	assert.Len(t, historyRows, 5)
+
+	// The on screen rows should always be the same, no matter which call is made
+	for i := range rows {
+		assert.Equal(t, rows[i], historyRows[i])
+	}
+
+	offset := -3
+	historyRows = term.History(offset)
+	curRow := offset - (len(historyRows) - 1)
+	for i, row := range historyRows {
+		line := fmt.Sprintf("line %d", curRow+i)
+		assert.True(t, strings.HasPrefix(row, line))
 	}
 }
